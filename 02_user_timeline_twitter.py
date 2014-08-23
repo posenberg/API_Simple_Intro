029e9d52921bd9df15a9c313db026e1c20a0706e
Mel Posenberg <posenberg@users.noreply.github.com>
2014-08-23 10:06:14 -0700

    Update 02_user_timeline_twitter.py

--- 02_user_timeline_twitter.py
+++ 02_user_timeline_twitter.py
@@ -1,15 +1,15 @@
 import tweepy
+
 import csv
-#Fill in your keys, secrets, and tokens from your app created on  https://apps.twitter.com/
+
 
 def monitor():
 
-
-
-	consumer_key = '538LEWAOqt8hAMgTdfqoY2J1b'
-	consumer_secret = 'xbSsONrS2mBhRn5ucU5XkKs6Mr4AK9FNKTsi07bDr4JJuEqHO5'
-	access_token = '787933-ZQ0ljPUD2hja9ENwHqmH63nmqwHYXfMWWujkBAonNiu'
-	access_token_secret = 'SfSeONIcOJ7DBuyOsoU5fN3NQTPqCYktC1bdwHWoLztR5'
+	#Fill in your keys, secrets, and tokens from your app created on  https://apps.twitter.com/
+	consumer_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+	consumer_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+	access_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+	access_token_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -28,17 +28,9 @@ def monitor():
 
 	csvFile = open('result.csv', 'w')
 	csvWriter = csv.writer(csvFile)
-
-	'''for tweet in new_tweets:
-		csvWriter.writerow(list(tweet.text.encode('utf-8')))
-		print i,' ', tweet.text.encode('utf-8')
-		print tweet.created_at
-		i=i+1'''
-
-	i=0
 	for tweet in new_tweets:
 		csvWriter.writerow([tweet.text.encode('utf-8')])
 		print tweet.created_at, ' ',tweet.text.encode('utf-8')
-
 	csvFile.close()
+	
 monitor()
