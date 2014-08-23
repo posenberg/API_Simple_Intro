0b9a1ce524f128d724e1d409a4ca8d047ccfae9e
Mel Posenberg <posenberg@users.noreply.github.com>
2014-08-23 09:58:53 -0700

    Update 03_post_to_twitter.py

--- 03_post_to_twitter.py
+++ 03_post_to_twitter.py
@@ -3,10 +3,10 @@ import tweepy
 
 def post_to_twitter():
 
-	consumer_key = '0MWtZ70TKubJqhXMYWfDtn1pc'
-	consumer_secret = 'AJTBAC79DAoCLA36Zfm32HT6UxnmfayMoBuKsF8qRzOcay2P4D'
-	access_token = '787933-5epwLWlO9ACbGiB9RkRdjlGlX9BoTCJbnFPAyYtOppa'
-	access_token_secret = '6SEOlHBTfhqmVKEtQ6VpCmZplUUr3G9gBbf21yFmnjSCf'
+	consumer_key = 'xxxxxxxxxxxxxxxxxxxx'
+	consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
